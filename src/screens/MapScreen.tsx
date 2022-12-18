1825079338ca499ad0233c9bf8abce8078f6112c
Piranja <techpiranja@gmail.com>
2022-12-18 18:41:48 +0100

    add geofencing and tts

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -9,17 +9,26 @@ import { ParkingSpotInfo } from '../types/ParkingSpotInfo';
 import ParkList from '../components/ParkList';
 import { ParkingSpot } from '../types/ParkingSpot';
 import ParkInfo from '../components/ParkInfo';
+import * as TaskManager from 'expo-task-manager';
+import * as Speech from 'expo-speech';
+import { LocationRegion } from 'expo-location';
+import { useToast } from 'native-base';
+
+const GEOFENCING = 'GEOFENCING';
 
 export default function MapScreen() {
   const [userLocation, setUserLocation] = useState<any>(null);
   const [mapLocation, setMapLocation] = useState<any>(null);
   const [errorMsg, setErrorMsg] = useState<any>(null);
+  const [loading, setLoading] = useState<boolean>(true);
   const { parkingSpots, changeSpot } = useParkingData();
   let mapRef = useRef<any>(null); // ref => { current: null }
+  const toast = useToast();
 
   useEffect(() => {
     (async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
+      await Location.requestBackgroundPermissionsAsync();
       if (status !== 'granted') {
         setErrorMsg('Permission to access location was denied');
         return;
@@ -29,7 +38,44 @@ export default function MapScreen() {
       setUserLocation(location);
       setMapLocation(location);
     })();
+
+    TaskManager.defineTask(GEOFENCING, ({ data: { eventType, region }, error }: any) => {
+      if (error) {
+        // check `error.message` for more details.
+        return;
+      }
+      if (eventType === Location.GeofencingEventType.Enter) {
+        console.log("You've entered region:", region);
+        toast.show({
+          description: 'Parkhaus ' + region.identifier + ' in der Nähe!'
+        });
+        Speech.speak('Parkhaus ' + region.identifier + ' in der Nähe!', { language: 'de' });
+      } else if (eventType === Location.GeofencingEventType.Exit) {
+        //console.log("You've left region:", region);
+      }
+    });
+    Speech.speak('Parkhaus in Nähe!');
   }, []);
+
+  useEffect(() => {
+    if (loading === false) {
+      const regions: LocationRegion[] = [];
+      parkingSpots.map((x) =>
+        regions.push({
+          identifier: x.name,
+          latitude: x.latitude,
+          longitude: x.longitude,
+          radius: 300,
+          notifyOnEnter: true
+        })
+      );
+      Location.startGeofencingAsync(GEOFENCING, regions);
+    }
+  }, [loading]);
+
+  useEffect(() => {
+    if (loading) setLoading(false);
+  }, [parkingSpots]);
 
   function moveToCoordinate(x: ParkingSpot) {
     mapRef.current.animateCamera({
@@ -88,7 +134,11 @@ export default function MapScreen() {
           strokeColor="rgba(0, 0, 0, 0)"
         />
       </MapView>
-      <ParkList parkingSpots={parkingSpots} moveToCoordinate={moveToCoordinate} changeSpot={changeSpot}/>
+      <ParkList
+        parkingSpots={parkingSpots}
+        moveToCoordinate={moveToCoordinate}
+        changeSpot={changeSpot}
+      />
     </View>
   );
 }
